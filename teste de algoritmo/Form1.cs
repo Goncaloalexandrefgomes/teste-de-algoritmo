ac3672ab4d272288d90745af75d934f1c8b2d011
goncalogomes <goncalogomes@WIN-GESFLHLR4EE>
2019-12-16 15:47:02 +0000

    5

--- teste de algoritmo/Form1.cs
+++ teste de algoritmo/Form1.cs
@@ -17,37 +17,31 @@ namespace teste_de_algoritmo
             InitializeComponent();
         }
 
+        private int[] sorteia(int qtd, int max)
+        {
+            Random rnd = new Random();
+            List<int> lista = new List<int>();
+            for (int i = 0; i < qtd; i++)
+            {
+                int tmp;
+                do tmp = rnd.Next(1, max + 1);
+                while (lista.Contains(tmp));
+                lista.Add(tmp);
+            }
+            lista.Sort();
+            return lista.ToArray();
+        }
+
         private void Button1_Click(object sender, EventArgs e)
         {
-            List<int> n = new List<int>();
-            Random rnd = new Random();
-            for (int i = 0; i < 5; i++)
-            {
-
-                int tmp;
-                do tmp = rnd.Next(1, 51);
-                while (n.Contains(tmp));
-                n.Add(tmp);
-            }
+            int[] n = sorteia(5, 50);
+            int[] s = sorteia(2, 12);
 
             n1.Text = n[0].ToString();
             n2.Text = n[1].ToString();
             n3.Text = n[2].ToString();
             n4.Text = n[3].ToString();
             n5.Text = n[4].ToString();
-
-
-
-            List<int> s = new List<int>();
-            for (int i = 0; i < 2; i++)
-            {
-
-                int tmp;
-                do tmp = rnd.Next(1, 13);
-                while (s.Contains(tmp));
-                s.Add(tmp);
-            }
-
             e1.Text = s[0].ToString();
             e2.Text = s[1].ToString();
         }
